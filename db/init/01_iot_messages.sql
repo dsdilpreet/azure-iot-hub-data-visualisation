6ddba36b5f4c5afe6482a2ec977868b9f6cb7754
dsdilpreet <dil_singh@outlook.com>
2026-07-03 12:35:19 +1200

    create iot_messages table

--- db/init/01_iot_messages.sql
+++ db/init/01_iot_messages.sql
@@ -1,18 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb;
 
 CREATE TABLE IF NOT EXISTS iot_messages (
-    message_time TIMESTAMPTZ NOT NULL,
-    enqueued_time TIMESTAMPTZ NULL,
-    device_id TEXT NULL,
-    blob_name TEXT NULL,
-    blob_time TIMESTAMPTZ NULL,
-    body JSONB NOT NULL,
+    enqueued_time TIMESTAMPTZ NOT NULL,
+    iothub_creation_time TIMESTAMPTZ NULL,
+    connection_device_id TEXT NULL,
+    body JSONB NULL,
     properties JSONB NULL,
     system_properties JSONB NULL,
     inserted_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-SELECT create_hypertable('iot_messages', 'message_time', if_not_exists => TRUE, migrate_data => TRUE);
+SELECT create_hypertable('iot_messages', 'enqueued_time', if_not_exists => TRUE, migrate_data => TRUE);
 
-CREATE INDEX IF NOT EXISTS ix_iot_messages_device_time ON iot_messages (device_id, message_time DESC);
+CREATE INDEX IF NOT EXISTS ix_iot_messages_device_time ON iot_messages (connection_device_id, enqueued_time DESC);
 CREATE INDEX IF NOT EXISTS ix_iot_messages_enqueued_time ON iot_messages (enqueued_time DESC);
